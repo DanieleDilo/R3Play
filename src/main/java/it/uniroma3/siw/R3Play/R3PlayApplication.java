b8008b92770aa74903a1ee2aae47026a1ecb1305
DanieleDilo <dan.dilorenzo2@stud.uniroma3.it>
2026-03-31 20:02:30 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/R3Play/R3PlayApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.R3Play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R3PlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R3PlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/R3Play/R3PlayApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.R3Play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R3PlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
