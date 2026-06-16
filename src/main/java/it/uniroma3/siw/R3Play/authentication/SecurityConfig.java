2e48575c4a932bb0e4cf1e70a6f16c9c3c4b5eb7
DanieleDilo <dan.dilorenzo2@stud.uniroma3.it>
2026-06-16 17:37:33 +0200

    fix recensioni

--- src/main/java/it/uniroma3/siw/R3Play/authentication/SecurityConfig.java
+++ src/main/java/it/uniroma3/siw/R3Play/authentication/SecurityConfig.java
@@ -22,6 +22,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .csrf(csrf -> csrf
+                .ignoringRequestMatchers("/api/**")
+            )
             .authorizeHttpRequests(auth -> auth
                 // Rotte protette - prima delle pubbliche
                 .requestMatchers(
